d2af3de76a48e1bf9a2ea5f90a0fdcf193c54f09
Nick Redmond <nickredmond@Nicks-MacBook-Pro.local>
2017-05-18 10:53:20 -0600

    adding questions as constants

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,1 +1,163 @@
+var QUESTIONS = [
+	{
+		"question_text": "",
+		"possible_answers": [
+			"",
+			"",
+			"",
+			""
+		],
+		"correct_answer_index":
+	}
 
+	{
+		"question_text": "When was Nick Redmond born?",
+		"possible_answers": [
+			"January 20, 1994 @ 6:55 P.M. Arizona Time",
+			"February 20, 1994 @ 6:55 P.M. Arizona Time",
+			"February 20, 1994 @ 1:32 A.M. Arizona Time",
+			"Febuary 22, 1994 @ 1:32 A.M. Arizona Time"
+		],
+		"correct_answer_index": 2
+	},
+	{
+		"question_text": "What is the maiden name of Nick Redmond's biological mother?",
+		"possible_answers": [
+			"Linda Lou Diggins",
+			"Linda Lou Diggs",
+			"Linda Leanne Diggins",
+			"Linda Leanne Diggs"
+		],
+		"correct_answer_index": 1
+	},
+	{
+		"question_text": "How many U.S. states has Nick Redmond visited?",
+		"possible_answers": [
+			"5",
+			"25",
+			"39",
+			"All 50"
+		],
+		"correct_answer_index": 1
+	},
+	{
+		"question_text": "How many full days did Nick Redmond spend at Colorado River Regional Youth Services?",
+		"possible_answers": [
+			"27",
+			"45",
+			"62",
+			"90"
+		],
+		"correct_answer_index": 0
+	},
+	{
+		"question_text": "When did Nick Redmond graduate from college?",
+		"possible_answers": [
+			"April 30, 2016",
+			"March 22, 2016",
+			"March 25, 2015",
+			"March 18, 2015"
+		],
+		"correct_answer_index": 3
+	},
+	{
+		"question_text": "What is Nick Redmond's favorite food?",
+		"possible_answers": [
+			"Burgers",
+			"Pizza",
+			"Spaghetti",
+			"All of the above"
+		],
+		"correct_answer_index": 3
+	},
+	{
+		"question_text": "What is Nick Redmond's dream job?",
+		"possible_answers": [
+			"Software Architect",
+			"Politician",
+			"Public Service Employee",
+			"Lawyer"
+		],
+		"correct_answer_index": 2
+	},
+	{
+		"question_text": "In which Arizona cities has Nick Redmond lived?",
+		"possible_answers": [
+			"Phoenix, Prescott, Kingman",
+			"Phoenix, Tempe, Chandler, Prescott, Kingman",
+			"Phoenix, Tempe, Chandler, Prescott, Kingman, Lake Havasu City",
+			"Phoenix, Tempe, Chandler, Prescott, Kingman, Bullhead City"
+		],
+		"correct_answer_index": 2
+	},
+	{
+		"question_text": "In which Utah cities has Nick Redmond lived?",
+		"possible_answers": [
+			"South Jordan, Salt Lake City, West Point",
+			"South Jordan, Salt Lake City, Orem, West Valley City, West Point",
+			"South Jordan, Salt Lake City, West Valley City, West Point",
+			"South Jordan, Salt Lake City, Provo, West Valley City, West Point"
+		],
+		"correct_answer_index": 1
+	},
+	{
+		"question_text": "In which California cities has Nick Redmond lived?",
+		"possible_answers": [
+			"Escondido, Wildomar",
+			"Wildomar",
+			"Del Mar, Los Angeles",
+			"Escondido, Los Angeles"
+		],
+		"correct_answer_index": 0
+	},
+	{
+		"question_text": "What is [truly] Nick Redmond's favorite song?",
+		"possible_answers": [
+			"\"Let's Go\" by Trick Daddy because Nick just can't help but \"get real wit' it\", sometimes",
+			"\"Footloose\" by Kenny Loggins because the song inspired Nick's passion for dancing",
+			"\"Should've Been a Cowboy\" by Toby Keith because Nick can empathize with the longing for rodeos",
+			"\"Dancing Queen\" by Abba because Nick's mom caught him dancing to it while playing outfield"
+		],
+		"correct_answer_index": 3
+	},
+	{
+		"question_text": "Which sports has Nick Redmond played, as part of a team?",
+		"possible_answers": [
+			"Basketball, Football, Wrestling",
+			"Baseball, Wrestling",
+			"Hockey, Football, Wrestling",
+			"Baseball, Football, Wrestling"
+		],
+		"correct_answer_index": 3
+	},
+	{
+		"question_text": "What are Nick Redmond's favorite pastimes?",
+		"possible_answers": [
+			"Snowboarding, Skydiving",
+			"Snowboarding, Playing Guitar, Helping Others",
+			"Snowboarding, Playing Guitar, Table Tennis, Eating, Helping Others",
+			"Drinking Beer, Barbeques, Making Fun of People, Watching Republican News"
+		],
+		"correct_answer_index": 2
+	},
+	{
+		"question_text": "Who was Nick Redmond's first-grade teacher?",
+		"possible_answers": [
+			"Mrs. Jonovich",
+			"Ms. Newman",
+			"Mr. DeWees",
+			"Mr. Tyree"
+		],
+		"correct_answer_index": 1
+	},
+	{
+		"question_text": "",
+		"possible_answers": [
+			"",
+			"",
+			"",
+			""
+		],
+		"correct_answer_index":
+	}
+];
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,20 @@
+function getSelectedAnswer() {
+	var selectedAnswer = document.querySelector("input[name='nr-question']:checked");
+	return selectedAnswer ? selectedAnswer.value : null;
+}
 
+function getNextQuestion(answer) {
+	
+}
+
+function onAnswerSubmit() {
+	var answer = getSelectedAnswer();
+
+	if (answer) {
+		document.getElementById("no-answer-alert").style.display = "none";
+		getNextQuestion(answer);
+	}
+	else {
+		document.getElementById("no-answer-alert").style.display = "block";
+	}
+}
